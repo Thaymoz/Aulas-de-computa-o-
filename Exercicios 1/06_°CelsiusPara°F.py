b881b0e593f4fcd57b80f82d26eef5ea08b53d1b
thaymoz <artemiorabello123@gmail.com>
2024-09-01 20:10:57 -0300

    Tudo feito

---
+++ Exercicios 1/06_°CelsiusPara°F.py
@@ -0,0 +1,7 @@
+# Faça um programa que peça um valor de temperatura em Celsius e mostre o valor convertido
+# para Fahrenheit. (cálculo para Fahrenheit: F = 9*C/5+32)
+
+c = float(input("Qual a temperatura em °C?"))
+conta = 9*c/5+32
+
+print (c,"celsius, vale", conta, "fahrenheit")
