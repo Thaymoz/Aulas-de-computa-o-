3932f815bcee22a5fdff941360b2296325295a5c
thaymoz <artemiorabello123@gmail.com>
2024-09-01 19:59:05 -0300

    Tudo feito

---
+++ Exercicios 1/04_TextoPersonalizado.py
@@ -0,0 +1,8 @@
+# Faça um programa que peça o nome, idade e altura de uma pessoa e imprima os valores,
+# formatados, com uma mensagem personalizada
+
+nome = input ("Qual o seu nome?")
+idade = int(input ("Quantos anos vc tem"))
+altura = float(input("Qual a sua altura(em metros)?"))
+
+print ("Hum interessante,",nome,"tendo ", idade, "anos, eu esperava que voce fosse um pouco mais alto do que", altura,"m ,mas tudo bem todos tem seus defeitos")
