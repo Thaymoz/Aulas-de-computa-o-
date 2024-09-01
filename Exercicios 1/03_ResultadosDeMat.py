7d97e5f1828ea821dd59bc9bf68ca233a19e8e1a
thaymoz <artemiorabello123@gmail.com>
2024-09-01 19:48:50 -0300

    Tudo feito

---
+++ Exercicios 1/03_ResultadosDeMat.py
@@ -0,0 +1,7 @@
+# Faça um programa com duas variáveis de números inteiros, atribua valores a elas e imprima o
+# resultado da soma, subtração, multiplicação e divisão entre elas.
+
+num1 = int(input("atribua valor ao primeiro numero"))
+num2 = int(input("atribua valor ao segundo numero"))
+
+print("A soma dos numeros que voce escolheu deu:",num1 + num2,"A subtraçao dos numeros que voce escolheu deu:",num1 - num2,"A divisao dos numeros que voce escolheu deu:",num1 / num2,"A mutiplicaçao dos numeros que voce escolheu deu:",num1 * num2,)
