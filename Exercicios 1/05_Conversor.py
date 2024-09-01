efd11326f7aecf653486581c6d9877bc5dbc237a
thaymoz <artemiorabello123@gmail.com>
2024-09-01 20:06:27 -0300

    Tudo feito

---
+++ Exercicios 1/05_Conversor.py
@@ -0,0 +1,7 @@
+# Faça um programa que peça um valor em metros e o mostre-o convertido em milímetros
+
+print("Bem vinde, ao melhor conversor do mundo de metros para milimet ros")
+num1 = float(input("Denomine o valor de metros"))
+conta = num1 * 1000
+
+print (num1,"metros, equivale à",conta)
