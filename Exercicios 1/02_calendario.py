bccfd4b9e9f1e9e2ba1f56ea7cc20f79f8d53cc1
thaymoz <artemiorabello123@gmail.com>
2024-09-01 19:41:30 -0300

    Exercicio feito

---
+++ Exercicios 1/02_calendario.py
@@ -0,0 +1,11 @@
+# Faça um programa que tenha as variáveis de dia, mês e ano e imprima, formatando a data com o
+# nome da cidade e “/” entre as variáveis.
+
+#Input é um comando que server para o computador entender o que o usario esta escrevendo
+#Int / Float serve para que a mensagem que o usario escreveu caso seja um numero converta ele
+#para um numero mesmo --> Int numeros inteiros / Float numeros quebrados
+dia = int(input("coloque o dia"))
+mes = int(input("coloque o mes"))
+ano = int(input("coloque o ano"))
+
+print("hoje em campinas é:",dia,"/",mes,"/",ano)
