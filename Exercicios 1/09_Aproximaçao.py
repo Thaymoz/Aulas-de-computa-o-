f1cc54819ff695ae3e4a41c1e673800ee201cbde
thaymoz <artemiorabello123@gmail.com>
2024-09-01 20:52:37 -0300

    Tudo feito

---
+++ Exercicios 1/09_Aproximaçao.py
@@ -0,0 +1,11 @@
+# Faça um programa com duas variáveis de números inteiros, atribua valores de 5 e 3 a elas.
+# Imprima o resultado da divisão aproximada delas. Dica: utilize a função round()
+
+a = 5
+b = 3
+
+#Round serve para arredondar o resultado de uma conta, logo vc deve colocar a conta dentro da funçao
+
+conta = round(a/b)
+
+print (conta)
