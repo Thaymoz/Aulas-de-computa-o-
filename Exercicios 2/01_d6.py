f8952d439f4560a5c1170403435cb59274e8c8c1
thaymoz <artemiorabello123@gmail.com>
2024-09-01 21:03:47 -0300

    D6

---
+++ Exercicios 2/01_d6.py
@@ -0,0 +1,8 @@
+# Faça um dado D6 e imprima o valor aleatório sorteado. Dica: utilize o módulo random e a função randint()
+
+#Import random -> serve para importar a biblioteca da "aleatoriedade"
+#random.randint->serve para ele escolher um numero aleatorio entre dois numeros que eu decidir
+import random
+d6 = random.randint(1,6)
+
+print(d6)
