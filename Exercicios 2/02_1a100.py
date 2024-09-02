42628479309e047b7e8f484d3ec4aaf58d115a98
thaymoz <artemiorabello123@gmail.com>
2024-09-01 22:48:52 -0300

    soma de 1 a 100

--- Exercicios 2/02_1a100.py
+++ Exercicios 2/02_1a100.py
@@ -3,9 +3,10 @@
 # choice() do módulo random
 
 import random
-#Quando colocado o range ele cria uma "lista" de numeros, ou seja ali ele vai criar os numeros de 1 a 100
+#Quando colocado o range ele cria uma "lista" de numeros[lembre sempre q o range vc tem q ser mais 1], 
+# ou seja ali ele vai criar os numeros de 1 a 100
 #Random.choice serve para escolher um numero aleatorio de uma lista q VC definiu
-list = range(1,100)
+list = range(1,101)
 d100 = random.choice(list)
 
 print (d100)
---
+++ Exercicios 2/05_Soma1a100.py
@@ -0,0 +1,5 @@
+# Calcule a soma dos números de 1 a 100 e exiba o resultado.
+
+list = range(1,101)
+print (sum(list))
+    
