85e7a5fdfaba31f8141d787c837f98757ed5e920
thaymoz <artemiorabello123@gmail.com>
2024-09-01 22:55:42 -0300

    Tabuada(Falta anotaçao)

---
+++ Exercicios 2/06_tabuada.py
@@ -0,0 +1,10 @@
+# Peça ao usuário para inserir um número e exiba a tabuada desse número de 1 a 10. 
+# Dica: Use um loop for em um
+# range e a operação de multiplicação
+
+num = float(input("Coloque um numero"))
+
+#For i in -> é um tipo de loop
+
+for i in range(1,11):
+    print (num*i)
