4f490bd1f6ce76bab151799fa92cf9d5bd7ca604
thaymoz <artemiorabello123@gmail.com>
2024-09-03 18:45:11 -0300

    Soma dos imapres feito

--- Exercicios 2/08_Impares com soma.py
+++ Exercicios 2/08_Impares com soma.py
@@ -7,10 +7,12 @@
 
 # print(rlist-rlist2)
 
-
 # for i in range(1,301,2):
 #     list=range(i)
 # print(sum(list))
+
+#Voce cria uma lista, dps cria um loop que no alcance de 1 a 301 a cada dois numeros o q da em numeros 
+# impares, voce ira somar todos os valores de i em uma lista q nao ha nada definido
 
 list = 0
 for i in range(1,301,2):
