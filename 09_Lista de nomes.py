2f6527c492f8dc691b61c1668ad13f90bb606596
thaymoz <artemiorabello123@gmail.com>
2024-09-03 18:57:42 -0300

    Lista de nomes feito

---
+++ 09_Lista de nomes.py
@@ -0,0 +1,9 @@
+# Crie uma lista com 3 nomes, peça um nome para o usuário e diga se este nome está contido ou não na lista.
+
+list = ("timo","tamires","caroline")
+nome = input("Digite um nome de um usuario do 330:")
+
+if nome in list:
+    print ("Voce esta certo", nome,"esta na lista")
+else:
+    print ("Essa pessoa nao é clt")
