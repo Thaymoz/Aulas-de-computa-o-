6b09f2d95fec7f10fcb05933c8b42c5568f4fddd
thaymoz <artemiorabello123@gmail.com>
2024-09-19 20:04:17 -0300

    Maiúsculo tudo feito

---
+++ Exercicios 3/02_maiusculo.py
@@ -0,0 +1,7 @@
+# Crie uma mensagem de boas vindas e formate-a em maiúsculo
+
+text = input ("Digite o texto q quiser ")
+
+#Bem aqui é bem simples upper serve para deixar as coisas em caixa maiuscula 
+#eu posso tanto deixar ali do lado de fora ou colocar ali junto do text formatado
+print(f'vc quis dizer {text}?'.upper())
