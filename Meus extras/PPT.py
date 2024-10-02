8c5a752741c810e3d0f91a5f33ed54785699f4d6
thaymoz <artemiorabello123@gmail.com>
2024-10-01 23:08:37 -0300

    Par ou impar tudo OK

--- Meus extras/PPT.py
+++ Meus extras/PPT.py
@@ -1,34 +1,22 @@
 #texto inicial 
-print ("Seja bem vindo jogador, vamos jogar jokenpo? Lembre-se de colocar as respostas com a primeira letra maiuscula")
+print ("Seja bem vindo jogador, vamos jogar Par ou Impar?")
 
 import random
 
-# jogador = input("Digite o q vc quer jogar")
-# list = ("Pedra","Papel","Tesoura")
-# computador = random.choice(list)
-
-# if jogador == computador:
-#     print ("voce jogou",jogador, "e o computador escolheu"computador,"empatou")
-# elif jogador:
-#     print("voce jogou",jogador, "e o computador escolheu"computador,"empatou")
+escolha = input("Par ou Impar? ").strip().upper()
 
 list = range(1,11)
 jogador = int(input("Digite um numero"))
 computador = random.choice(list)
 calculo = jogador + computador
 
-try:
-    escolha = input("Par ou Impar? ").strip().upper()
-    if escolha == ("PAR"):
-        if calculo % 2 == 0: 
-            print ("vc ganhou",calculo)
-        else:
-            print ("vc perdeu",calculo)
-    elif escolha == ("IMPAR"):
-        if calculo % 2 != 0: 
-            print ("vc ganhou",calculo)
-        else:
-            print ("vc perdeu",calculo)
-
-except NameError:
-     print("Escolhe par ou impar seu burro")
+if escolha == ("PAR"):
+    if calculo % 2 == 0: 
+        print ("vc ganhou",calculo)
+    else:
+        print ("vc perdeu",calculo)
+elif escolha == ("IMPAR"):
+    if calculo % 2 != 0: 
+        print ("vc ganhou",calculo)
+    else:
+        print ("vc perdeu",calculo)
